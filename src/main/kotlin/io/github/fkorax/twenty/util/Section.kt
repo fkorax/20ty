a0dd4cf4f919daea92ea829564aa0743eb39683e
Franchesko Korako <franchesko.korako@gmail.com>
2022-08-31 22:35:27 +0200

    Add sections
    Sections are a quick way to isolate use-once subroutines (which are
    only called once, and only by their surrounding routines, i.e. no code
    is / can be reused in them) from their surrounding code, resulting in a
    more structured program without creating lots of private methods which
    are only used to isolate parts of one code path from each other.
    
    Code sections are implemented in Kotlin with an inline function, which
    takes a name and a closure block with no arguments returning Unit.
    The String name is optimized away at compile time and the function is
    inlined, leaving no trace of sectioning in the compiled output.

---
+++ src/main/kotlin/io/github/fkorax/twenty/util/Section.kt
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2022  Franchesko Korako
+ *
+ * This file is part of 20ty.
+ *
+ * 20ty is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * 20ty is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with 20ty.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package io.github.fkorax.twenty.util
+
+inline fun section(@Suppress("UNUSED_PARAMETER") name: String, block: () -> Unit) {
+    // Contracts will be used when they are no longer an experimental feature
+    /*
+    contract {
+        callsInPlace(block, InvocationKind.EXACTLY_ONCE)
+    }
+    */
+    block()
+}
