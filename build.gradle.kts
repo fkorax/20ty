37963df36245932f836e97856c697415be2f017c
Franchesko Korako <franchesko.korako@gmail.com>
2022-09-09 18:22:20 +0200

    Migrate from IntelliJ to Gradle
    * Add Gradle configuration files
    * Add Gradle wrapper files
    * Update IDEA files
    * Remove obsolete IDEA files
    * Update .gitignore

---
+++ build.gradle.kts
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) 2022  Franchesko Korako
+ *
+ * This file is part of 20ty.
+ *
+ * 20ty is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * 20ty is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with 20ty.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.7.10"
+    application
+    idea
+}
+
+group = "io.github.fkorax"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+application {
+    mainClass.set("io.github.fkorax.twenty.Twenty")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
